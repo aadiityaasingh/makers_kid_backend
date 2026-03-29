6231876e446a4b407883b5eb1e35948b4ba597fb
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-29 14:55:49 +0530

    done

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://localhost:5175",
   "http://localhost:5176",
-  "https://your-frontend.onrender.com"
+  "https://maker-kids.vercel.app/"
 ];
 
 app.use(
